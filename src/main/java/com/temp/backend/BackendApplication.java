531f768d64b19ad1196ae64ce4cd41e1b2c157a0
xabinM <binine2@gmail.com>
2025-12-16 17:22:36 +0900

    feat: spring cloud & eureka 설정 추가

--- src/main/java/com/temp/backend/BackendApplication.java
+++ src/main/java/com/temp/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.temp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BackendApplication {
 
